49245d16cba257f6366cae3622519ea6c15f121b
zannatrimi <zannatrimi.cse@gmail.com>
2015-02-02 10:19:58 +0600

    Add function

---
+++ Calculator/function.js
@@ -0,0 +1,37 @@
+/**
+ * Created by zannatul.ferdous on 1/27/15.
+ */
+
+
+
+function c(val)
+{
+    document.getElementById("demo").value=val;
+}
+function v(val)
+{
+    document.getElementById("demo").value+=val;
+}
+function s(){
+    document.getElementById("demo").value = Math.sqrt();
+}
+function t(){
+    function v(){ function s(){}}
+}
+function e()
+{
+    try
+    {
+        c(eval(document.getElementById("demo").value))
+    }
+    catch(e)
+    {
+        c('Error')
+    }
+}
+
+
+
+
+
+
